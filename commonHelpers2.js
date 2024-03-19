2873ec9d430d01c3d65ed50aa267636eed83b4f3
userDmytro <104132845+userDmytro@users.noreply.github.com>
2024-03-19 00:38:25 +0000

    Deploying to gh-pages from @ userDmytro/goit-js-hw-10@458aa400dceebd3fe4ae6386ec8d0c67fcc01b91 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-b5abda9d.js";import{i as s}from"./assets/vendor-77e16229.js";const l=document.querySelector(".form");l.addEventListener("submit",o=>{o.preventDefault();const e=o.currentTarget.elements.delay.value,t=o.currentTarget.elements.state.value;new Promise((r,i)=>{setTimeout(()=>{console.log(e,t),t==="fulfilled"?r():i()},e)}).then(r=>{s.success({color:"green",position:"topRight",message:`✅ Fulfilled promise in ${e}ms`})}).catch(r=>{s.error({color:"red",position:"topRight",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
